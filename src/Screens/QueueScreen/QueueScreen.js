b31db0625bfa49ebff3a285709996da56a676bc2
imobidjon <imobidjon@gmail.com>
2023-11-20 08:45:11 +0500

    added some design in queue

--- src/Screens/QueueScreen/QueueScreen.js
+++ src/Screens/QueueScreen/QueueScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { listQueue } from "../../actions";
 import { Store } from "../../Store";
-import { Alert, CircularProgress, Grid, List, ListItem, Paper } from "@mui/material";
+import { Alert, CircularProgress, Divider, Grid, List, ListItem, Paper } from "@mui/material";
 
 export default function QueueScreen() {
   const { state, dispatch } = useContext(Store);
@@ -10,6 +10,7 @@ export default function QueueScreen() {
   useEffect(() => {
     listQueue(dispatch);
   }, [dispatch]);
+
   return (
     <div>
       {loading ? (
@@ -18,26 +19,32 @@ export default function QueueScreen() {
         <Alert severity="error">{error}</Alert>
       ) : (
         <Grid container spacing={2}>
-          <Grid item md={6}>
-            <Paper>
-              <h1 >In Progress</h1>
+          <Grid item md={6} xs={6}>
+            <Paper sx={{textAlign: 'center'}}>
+              <h1 style={{background: 'red', color: 'white', padding: '10px' }}>Tayyorlanmoqda</h1>
               <List>
                 {queue.inProgressOrders.map((order) => (
-                  <ListItem key={order.number}>
-                    <h1>{order.number}</h1>
-                  </ListItem>
+                  <>
+                    <ListItem sx={{display: 'flex', justifyContent: 'center'}} key={order.number}>
+                      <h1>{order.number}</h1>
+                    </ListItem>
+                    <Divider />
+                  </>
                 ))}
               </List>
             </Paper>
           </Grid>
-          <Grid item md={6}>
-            <Paper>
-              <h1>Now Serving </h1>
+          <Grid item md={6} xs={6}>
+            <Paper sx={{textAlign: 'center'}}>
+              <h1 style={{background: 'green', color: 'white', padding: '10px' }}>Tayyor </h1>
               <List>
                 {queue.servingOrders.map((order) => (
-                  <ListItem key={order.number}>
-                    <h1>{order.number}</h1>
-                  </ListItem>
+                  <>
+                    <ListItem sx={{display: 'flex', justifyContent: 'center'}} key={order.number}>
+                      <h1>{order.number}</h1>
+                    </ListItem>
+                    <Divider />
+                  </>
                 ))}
               </List>
             </Paper>
